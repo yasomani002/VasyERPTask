8048961b37ebe2dcde8df499384fa288b9ccdff0
Yash Somani <yashsomani231@gmail.com>
2025-10-16 17:00:59 +0530

    fix tailwind css bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}" // <- scan all your components
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
